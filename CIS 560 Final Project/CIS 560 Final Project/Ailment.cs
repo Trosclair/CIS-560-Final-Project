14f720bc106dc9e219d3e1b1d9fa3c53e18ba2d1
Trosclair <30909416+Trosclair@users.noreply.github.com>
2019-04-23 10:54:58 -0500

    Ailment class added I think all the easy stuff is done now :(

---
+++ CIS 560 Final Project/CIS 560 Final Project/Ailment.cs
@@ -0,0 +1,22 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS_560_Final_Project
+{
+    public class Ailment
+    {
+        public int AilmentId { get; }
+        public string Name { get; }
+        public string Symptoms { get; }
+
+        internal Ailment(int ailmentId, string name, string symptoms)
+        {
+            AilmentId = ailmentId;
+            Name = name;
+            Symptoms = symptoms;
+        }
+    }
+}
