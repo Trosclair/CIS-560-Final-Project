470b31dd0ad70ec125f8d2181da646df59d7b841
Trosclair <30909416+Trosclair@users.noreply.github.com>
2019-04-23 10:19:24 -0500

    PetAilment class finished

---
+++ CIS 560 Final Project/CIS 560 Final Project/PetAilment.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS_560_Final_Project
+{
+    public class PetAilment
+    {
+        public int PetAilmentId { get; }
+        public int AilmentId { get; }
+        public int PetId { get; }
+        public bool IsActive { get; }
+
+        internal PetAilment(int petAilmentId, int ailmentId, int petId, bool isActive)
+        {
+            PetAilmentId = petAilmentId;
+            AilmentId = ailmentId;
+            PetId = petId;
+            IsActive = isActive;
+        }
+    }
+}
