c22062f6e6dc59d4a370ff6def8a0f57c143f2ed
Trosclair <30909416+Trosclair@users.noreply.github.com>
2019-04-23 10:03:47 -0500

    Clinic class finished

---
+++ CIS 560 Final Project/CIS 560 Final Project/Clinic.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS_560_Final_Project
+{
+    public class Clinic
+    {
+        public int ClinicId { get; }
+        public string Name { get; }
+
+        internal Clinic(int clinicId, string name)
+        {
+            ClinicId = clinicId;
+            Name = name;
+        }
+    }
+}
