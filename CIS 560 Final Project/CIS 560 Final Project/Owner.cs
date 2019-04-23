03f360cef345fba954e7b135a3c7218838ddb21a
Trosclair <30909416+Trosclair@users.noreply.github.com>
2019-04-23 09:53:50 -0500

    Owner finished

---
+++ CIS 560 Final Project/CIS 560 Final Project/Owner.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CIS_560_Final_Project
+{
+    public class Owner
+    {
+        public int OwnerId { get; }
+        public int ClinicId { get; }
+        public string PhoneNumber { get; }
+        public string FirstName { get; }
+        public string LastName { get; }
+        public int HappinessPercent { get; }
+
+        internal Owner(int ownerId, int clinicId, string phoneNumber, string firstName, string lastName, int happinessPercent)
+        {
+            OwnerId = ownerId;
+            ClinicId = clinicId;
+            PhoneNumber = phoneNumber;
+            FirstName = firstName;
+            LastName = lastName;
+            HappinessPercent = happinessPercent;
+        }
+    }
+}
